ccb146e2c55b7b4f971737173bbfbad773bbafdf
Raed Sbenaty <raeed.sbenaty.963@gmail.com>
2022-12-03 15:46:02 +0300

    frame subtraction

--- skin.py
+++ skin.py
@@ -23,15 +23,37 @@ def generate_skin_mask(img):
     return mask.astype(np.uint8) * 255
 
 
+def ycbcr_substract(img1, img):
+    diff = cv2.absdiff(img1, img)
+    diff = diff[:, :, 1] + diff[:, :, 2]
+    diff = (diff >= 10)*255
+    return diff.astype(np.uint8)
+
+
 def main():
     cap = cv2.VideoCapture(0)
+    bgFrame = None
+
     while True:
         _, img = cap.read()
-        mask = generate_skin_mask(img)
-        cv2.imshow('mask', mask)
-        cv2.imshow('img', img)
-        if cv2.waitKey(30) & 0xff == 27:
+        skin_mask = generate_skin_mask(img)
+
+        k = cv2.waitKey(30)
+        if k & 0xff == 27:
             break
+
+        elif k == ord('z') and bgFrame is None:
+            bgFrame = cv2.cvtColor(img, cv2.COLOR_BGR2YCrCb)
+
+        elif bgFrame is not None:
+            ycrcb_frame = cv2.cvtColor(img, cv2.COLOR_BGR2YCrCb)
+            diff = ycbcr_substract(ycrcb_frame, bgFrame)
+            final_result = diff & skin_mask
+            cv2.imshow("ycbcr_substract", diff)
+            cv2.imshow("skin_mask & ycbcr_substract", final_result)
+
+        cv2.imshow('skin_mask', skin_mask)
+        cv2.imshow('img', img)
 
 
 if __name__ == '__main__':
