ee33dc3a81d42cfa0e63b43da44e69e41bb899e9
__abada__ <54916786+aabadaa@users.noreply.github.com>
2022-12-15 18:22:31 +0300

    implement get_directions

---
+++ motion_direction.py
@@ -0,0 +1,24 @@
+from enum import Enum
+from Queue import Queue
+
+
+class Directions(Enum):
+    LEFT = "left"
+    RIGHT = "right"
+    UP = "up"
+    DOWN = "Down"
+
+
+def get_direction_from(q: Queue):
+    start, end = q.q[0], q.q[-1]
+    is_up = end[0] - start[0] < 0
+    is_left = end[1] - start[1] < 0
+    return (
+        Directions.LEFT if is_left else Directions.RIGHT,
+        Directions.UP if is_up else Directions.DOWN
+    )
+
+q = Queue(20)
+q.append((10,10))
+q.append((0,28))
+print(get_direction_from(q))
