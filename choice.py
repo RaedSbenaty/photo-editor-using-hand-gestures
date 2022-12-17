f0acdee3502894ed6555ab8e279788c1bd52a1b1
__abada__ <54916786+aabadaa@users.noreply.github.com>
2022-12-17 23:25:47 +0300

    fix input_mapper and add some new operations

--- choice.py
+++ choice.py
@@ -2,16 +2,18 @@ from enum import Enum
 
 
 class Choice(Enum):
-    NOTHING = 0
-    ROTATE = 1
-    TRANSLATE = 2
-    SCALE = 3
-    WARP = 4
-    SKEW = 5
-    PAINT = 6
-    CLEAR = 7
-    SAVE = 8
-    SELECT = 9
-    CLICK = 10 
-    SIZE_INC = 11 
-    SIZE_DEC = 12
+    NOTHING = 0 #
+    ROTATE = 1 # 
+    TRANSLATE = 2 # 
+    SCALE = 3 # 
+    WATER_MARK = 4 
+    SKEW = 5 # 
+    PAINT = 6 # 
+    CLEAR = 7 # 
+    SAVE = 8 # 
+    SELECT = 9 # 
+    CLICK = 10  # 
+    SIZE_INC = 11 # 
+    SIZE_DEC = 123 
+    COLOR_PICKER = 13 # 
+    UNDO = 14 #
